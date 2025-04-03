444b5d9e5183411f87533ed2b566ce9f62f75bc7
Đặng Xuân Minh Hiếu <zipdang2004@gmail.com>
2025-04-03 15:44:27 +0700

    split main to different problems

--- algorithms/AStar.h
+++ algorithms/AStar.h
@@ -12,17 +12,14 @@ class AStar: public SearchingAlgorithm<State> {
 			State _start = (this -> statement).getSource();
 			const State DESTINATION = (this -> statement).getDestination();
 			
-			std::map<State, double> *g = &(this -> g);
-			std::map<State, std::string> *actionTrace = &(this -> actionTrace);
-			
-			(*g)[_start] = 0; 
-			(*actionTrace)[_start] = "";
+			(this -> g)[_start] = 0; 
+			(this -> actionTrace)[_start] = "";
 			double initH = (this -> statement).heuristic(_start);
 			pq.emplace(_start, initH, 0, initH);
 
 			while (not pq.empty()) {
 				StateInfo<State> currentInfo = pq.top(); pq.pop();
-				if (currentInfo.g != (*g)[currentInfo.state]) continue;
+				if (currentInfo.g != (this -> g)[currentInfo.state]) continue;
 				if (currentInfo.state.isSolved()) {
 					this -> FINISH_SEARCHING();
 					break;
@@ -33,12 +30,12 @@ class AStar: public SearchingAlgorithm<State> {
 					double newG = currentInfo.g + cost, h = (this -> statement).heuristic(newState);
 					double newF = newG + h;
 
-					auto it = g -> find(newState);
-					if (it != g -> end() and it -> second <= newG)	// currently better
+					auto it = (this -> g).find(newState);
+					if (it != (this -> g).end() and it -> second <= newG)	// currently better
 						continue;
 
-					(*g)[newState] = newG;
-					(*actionTrace)[newState] = action;
+					(this -> g)[newState] = newG;
+					(this -> actionTrace)[newState] = action;
 					pq.emplace(newState, newF, newG, h);
 				}
 			}
---
+++ exec/NSqPuzzle.cpp
@@ -0,0 +1,49 @@
+#include "testlib.h"
+#include <bits/stdc++.h>
+#include "../statements/NSqPuzzle/Problem.h"
+#include "../algorithms/problems/NSqPuzzle.h"
+#include "../algorithms/Dijkstra.h"
+#include "../algorithms/AStar.h"
+#include "../algorithms/FocalSearch.h"
+#include "../algorithms/ProbabilisticFocalSearch.h"
+#include "../algorithms/AStarPE.h"
+#include "../algorithms/PFS_PE.h"
+// const int SIZE = 4;
+Board _INIT("NOT_INIT");
+
+int n;
+std::vector<int> state;
+
+void input() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin >> n; 
+	state.resize(n * n); Board::changeSize(n);
+	
+	for (int& i: state) std::cin >> i;
+}
+
+std::vector<Algorithm*> algorithms;
+int main(int argc, char**argv){
+	registerGen(argc, argv, 1);
+	input();
+
+	Problem<Board> statement = Problem<Board>(Board(state), Board());
+	algorithms.push_back(new AStar(statement));
+	algorithms.push_back(new AStarPartialExpansion(statement, 2));
+	algorithms.push_back(new FocalSearch(statement, 1.05));
+	algorithms.push_back(new ProbabilisticFocalSearch(statement, 1.05, 0.6));
+	algorithms.push_back(new ProbabilisticFocalSearch_PartialExpansion(statement, 1.05, 0.6, 2));
+	for (Algorithm *algorithm: algorithms) {
+		std::cerr << algorithm->getName() << std::endl;
+		std::cout << "start " << algorithm -> getName() << '\n';
+		double timeUsed = algorithm -> measure();
+		std::cout << "count of expanded nodes: " << ((SearchingAlgorithm<Board>*) algorithm) -> getExpandedCount() << '\n';
+		std::cout << "count of expanded iterations: " << ((SearchingAlgorithm<Board>*) algorithm) -> getIterationCount() << '\n';
+		std::cout << "steps: ";
+		for (std::string s: ((SearchingAlgorithm<Board>*) algorithm) -> getTrace()) std::cout << s;
+		std::cout << '\n';
+		// std::cout << "steps: " << ((PuzzleAlgorithm*) algorithm) -> makeStep() << '\n';
+		std::cout << "execution time: " << timeUsed << '\n';
+		algorithm -> ~Algorithm();
+	}
+}
