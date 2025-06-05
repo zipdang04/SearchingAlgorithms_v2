62a071696b49cadbe822e1369ad1c304f834132f
Đặng Xuân Minh Hiếu <zipdang2004@gmail.com>
2025-06-05 18:26:42 +0700

    change how it runs

--- experiments/run_separately.py
+++ experiments/run_separately.py
@@ -1,18 +1,42 @@
+import json
+
 import os, sys, subprocess
 import datetime
 datetime.datetime.now().timestamp()
 TEST_SETTINGS = {
-	0: 100
+	"{\"n\": 4, \"ctrl\": 50}": 50,
+	"{\"n\": 4, \"ctrl\": 60}": 50,
+	"{\"n\": 4, \"ctrl\": 70}": 50,
+	"{\"n\": 4, \"ctrl\": 80}": 50,
+	"{\"n\": 4, \"ctrl\": 90}": 50,
+	"{\"n\": 4, \"ctrl\": 100}": 50,
+	"{\"n\": 4, \"ctrl\": 120}": 50,
+	"{\"n\": 4, \"ctrl\": 150}": 50,
+	"{\"n\": 4, \"ctrl\": 180}": 50,
+	"{\"n\": 4, \"ctrl\": 200}": 50,
+	"{\"n\": 4, \"ctrl\": 0}": 500,
+	
+	"{\"n\": 5, \"ctrl\": 50}": 50,
+	"{\"n\": 5, \"ctrl\": 60}": 50,
+	"{\"n\": 5, \"ctrl\": 70}": 50,
+	"{\"n\": 5, \"ctrl\": 80}": 50,
+	"{\"n\": 5, \"ctrl\": 90}": 50,
+	"{\"n\": 5, \"ctrl\": 100}": 50,
+	"{\"n\": 5, \"ctrl\": 120}": 50,
+	"{\"n\": 5, \"ctrl\": 150}": 50,
+	"{\"n\": 5, \"ctrl\": 180}": 50,
+	"{\"n\": 5, \"ctrl\": 200}": 50,
+	"{\"n\": 5, \"ctrl\": 0}": 500,	
 }
 EXE = [
-	""
+	"../build/SearchingAlgorithmsV2"
 ]
 def currentDirectory() -> str:
 	return os.path.dirname(os.path.abspath(__file__))
 os.chdir(currentDirectory())
 
-def makeTest(ctrl: int) -> str:
-	command = ["../build/DataCreation", "--n", "4"]
+def makeTest(n: int, ctrl: int) -> str:
+	command = ["../build/DataCreation", "--n", str(n)]
 	if ctrl > 0:
 		command.append("--ctrl")
 		command.append(str(ctrl))
@@ -22,15 +46,19 @@ def makeTest(ctrl: int) -> str:
 sys.stdout = open("statistics"+str(int(datetime.datetime.now().timestamp() * 1000000))+".csv", "w")
 columns = ["n", "ctrl", "filename", "algo_name", "expanded_nodes", "iteration_count", "max_size", "time", "count_steps", "steps"]
 print(*columns, sep="\t")
-for ctrl in TEST_SETTINGS:
-	COUNT = TEST_SETTINGS[ctrl]
+print(TEST_SETTINGS)
+for key in TEST_SETTINGS:
+	_tmp = json.loads(key)
+	print(_tmp, file = sys.stderr)
+	n, ctrl = _tmp["n"], _tmp["ctrl"]
+	COUNT = TEST_SETTINGS[key]
 	for i in range(COUNT):
-		inputFile = makeTest(ctrl)
+		inputFile = makeTest(n, ctrl)
 		tmp = []
 		for PATH in EXE:
 			try:
 				fInput = open(inputFile, "r")
-				process = subprocess.run([PATH], stdin=fInput, capture_output=True, timeout=500)
+				process = subprocess.run([PATH], stdin=fInput, capture_output=True, timeout=600)
 				output = process.stdout.decode().split(sep = '\n')
 				for group in range(0, len(output), 6):
 					if (group + 4 >= len(output)): break
@@ -46,6 +74,7 @@ for ctrl in TEST_SETTINGS:
 					print(*data, sep = '\t')
 				print("ctrl = {} | done test {}".format(ctrl, i + 1), file=sys.stderr)
 			except subprocess.TimeoutExpired:
+				tmp.append(-1)
 				print("ctrl = {} | test {} is too hard ({})".format(ctrl, i + 1, inputFile), file=sys.stderr)
-			if len(tmp) > 0:
-				print(inputFile, *tmp, file=sys.stderr)
+		if len(tmp) > 0:
+			print(inputFile, *tmp, file=sys.stderr)
